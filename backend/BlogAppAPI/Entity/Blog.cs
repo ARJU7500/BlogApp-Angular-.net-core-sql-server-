6344d3cfb9d0e545978bdd4ce96e1c5133f70bde
Arju7500 <an096237@gmail.com>
2026-03-17 02:48:07 +0530

    all setup is completed in angular(project created+material css+tailwind css) ans dot net core(connect to database+entity(blog+blogcategory))

---
+++ backend/BlogAppAPI/Entity/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogAppAPI.Entity
+{
+    [Table("BLOG_MASTER")]
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Content { get; set; }
+        public string? image { get; set; }
+        public bool isFetured { get; set; }
+        public int IategryId { get; set; }
+        public Category Category { get; set; }
+        public DateTime CreatrdOn { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdatrdOn { get; set; }
+    }
+}
---
+++ backend/BlogAppAPI/Entity/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogAppAPI.Entity
+{
+    [Table("BLOG_CATEGORY")]
+    public class Category
+    {
+        public int BlogCategoryId { get; set; }
+        public string? CategotyName { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatrdOn { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdatrdOn { get; set; }
+    }
+}
