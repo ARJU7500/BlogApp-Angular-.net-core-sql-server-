11f9f6a7c0561ed5bee36735bc479a7fdca5c023
Arju7500 <an096237@gmail.com>
2026-03-19 01:25:23 +0530

    create pages

--- froent-end/BlogWebApp/src/app/app.routes.ts
+++ froent-end/BlogWebApp/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './Components/home/home';
+import { About } from './Components/about/about';
+import { Blog } from './Components/blog/blog';
+import { Blogs } from './Components/blogs/blogs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component:Home},
+    //{path:'**',redirectTo:'/'},
+    {path:'about',component:About},
+    {path: 'blog',component:Blog},
+    {path:'blogs',component:Blogs}
+];
--- froent-end/BlogWebApp/src/app/app.ts
+++ froent-end/BlogWebApp/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import { Header } from './Components/header/header';
+import { Footer } from './Components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatButtonModule],
+  imports: [RouterOutlet,MatButtonModule,Header,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
